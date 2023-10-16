3bbd093a65b5c386e609f61d598a266ca54bac7b
Sergey SVN <106481632+Egnat@users.noreply.github.com>
2023-10-16 15:42:30 +0300

    Add files via upload

--- public/script.js/main.js
+++ public/script.js/main.js
@@ -26,27 +26,6 @@ for (let i = 0; i < dots.length; i++) {
   dots[slideIndex-1].className += " active";
 }
 
-// Slider automat
-/*let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}*/
-
 //Слайдер Токаревской
 let images = [{
     url: "https://img.favcars.com/mini/hatch/mini_hatch_2010_wallpapers_14_1280x960.jpg",
@@ -76,8 +55,8 @@ let images = [{
     url:"https://images.unsplash.com/photo-1661630802936-5ff15855cd1f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjIwNjEyNjU&ixlib=rb-1.2.1&q=80&w=400",
     title:'buildings'
   }, {
-    url:'public/images/3-fig.png',/*public/ добавляем, когда смотрим не в реакте*/
-    title:'City2'
+    url:'images/3-fig.png',
+    title:'city'
   }
   ];
 
@@ -219,7 +198,7 @@ window.onclick = function(event) {
 const cardOff = document.querySelector('.cardOffFilterImgHeader');
 
 cardOff.addEventListener('click', () => {//function show
-  const photoOff = document.querySelector('.filterImgHeader');//Можно спрятать и вернуть любой элемент filterImgHeader 
+  const photoOff = document.querySelector('.filterImgHeader-2');//Можно спрятать и вернуть любой элемент filterImgHeader 
   if (photoOff.style.display == "block") {
      photoOff.style.display = "none";
       return true;
